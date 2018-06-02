30409ba8957bd7e05cfc1ea1fda04357bedd47b8
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-06-02 18:36:05 -0500

    working with electron dialog

--- app/main.js
+++ app/main.js
@@ -1,15 +1,35 @@
-const { app, BrowserWindow } = require('electron');
+// Modules from Electron Lib
+const { app, BrowserWindow, dialog } = require('electron');
 
 let mainWindow = null;
 
+const getFileFromUserSelection = () => {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties:['openFile'], 
+    filters: [
+      { name: 'Text Files', extensions: ['txt', 'text']},
+      { name: 'Markdown Files', extensions: ['md', 'markdown']}
+    ]
+  });
+
+  if (!files) return; // if the user hits cancel then don't blow up
+  
+  const file = files[0]; // get the first out of the array
+
+  console.log(files);
+};
+
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ show: false });
-
+  
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-
+  
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
+    getFileFromUserSelection();
   });
+  
+
 
   mainWindow.on('closed', () => {
     mainWindow = null;
--- app/renderer.js
+++ app/renderer.js
@@ -8,3 +8,13 @@ const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
 const saveHtmlButton = document.querySelector('#save-html');
+
+// create a reusable function to use elsewhere
+const renderMarkdownToHtml = (markdown) => {
+    htmlView.innerHTML = marked(markdown, { sanitize: true }); //sanitize the user inputs
+};
+
+// when a keyup happens in the markdown view (event listen)
+markdownView.addEventListener('keyup', (event) => {
+    renderMarkdownToHtml(event.target.value);
+});
